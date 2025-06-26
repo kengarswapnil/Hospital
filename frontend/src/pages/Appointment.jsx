ded7ed05aaaf034f832f91d0cc3cd169db3a0574
Swapnil Kengar <your-email@example.com>
2025-06-26 19:22:43 +0530

    working in project

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -5,19 +5,73 @@ import { assets } from "../assets/assets";
 
 const Appointment = () => {
   const { docId } = useParams();
-
   const { doctors, currencySymbol } = useContext(AppContext);
+  const dayofweek = ["SUN", "MON", "THU", "WEN", "THU", "FRI", "SAT"];
 
   const [docInfo, setDocInfo] = useState(null);
+  const [docSlots, setDocSlots] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState("");
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find((doc) => doc._id === docId);
     setDocInfo(docInfo);
-    console.log(docInfo);
+    // console.log(docInfo);
   };
+
+  const getAvailableSlots = async () => {
+    setDocSlots([]);
+
+    let today = new Date();
+
+    for (let i = 0; i < 7; i++) {
+      let currentDate = new Date(today);
+      currentDate.setDate(today.getDate() + i);
+
+      let endTime = new Date();
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
+
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(
+          currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10
+        );
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+      } else {
+        currentDate.setHours(10);
+        currentDate.setMinutes(0);
+      }
+
+      let timeSlots = [];
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        //add slots
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime,
+        });
+
+        //increment time
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
+      }
+      setDocSlots((prev) => [...prev, timeSlots]);
+    }
+  };
+
   useEffect(() => {
     fetchDocInfo();
   }, [doctors, docId]);
+
+  useEffect(() => {
+    getAvailableSlots();
+  }, [docInfo]);
+
+  useEffect(() => {
+    console.log(docSlots);
+  }, [docSlots]);
 
   return (
     docInfo && (
@@ -63,106 +117,49 @@ const Appointment = () => {
             </p>
           </div>
         </div>
+
+        {/* booking slots */}
         <div className="sm:ml-72 sm:pl-4 mt-8 font-medium text-[#565656]">
           <p>Booking slots</p>
           <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
-            <div className="text-center py-6 min-w-16 rounded-full cursor-pointer border border-[#DDDDDD]">
-              <p>MON</p>
-              <p>16</p>
-            </div>
-            <div className="text-center py-6 min-w-16 rounded-full cursor-pointer bg-primary text-white">
-              <p>TUE</p>
-              <p>17</p>
-            </div>
-            <div className="text-center py-6 min-w-16 rounded-full cursor-pointer border border-[#DDDDDD]">
-              <p>WED</p>
-              <p>18</p>
-            </div>
-            <div className="text-center py-6 min-w-16 rounded-full cursor-pointer border border-[#DDDDDD]">
-              <p>THU</p>
-              <p>19</p>
-            </div>
-            <div className="text-center py-6 min-w-16 rounded-full cursor-pointer border border-[#DDDDDD]">
-              <p>FRI</p>
-              <p>20</p>
-            </div>
-            <div className="text-center py-6 min-w-16 rounded-full cursor-pointer border border-[#DDDDDD]">
-              <p>SAT</p>
-              <p>21</p>
-            </div>
-            <div className="text-center py-6 min-w-16 rounded-full cursor-pointer border border-[#DDDDDD]">
-              <p>SUN</p>
-              <p>22</p>
-            </div>
+            {docSlots.length &&
+              docSlots.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    onClick={() => setSlotIndex(index)}
+                    className={`text-center py-6 min-w-16 rounded-full cursor-pointer border ${
+                      slotIndex === index
+                        ? "bg-primary text-white"
+                        : "border-[#DDDDDD]"
+                    }`}
+                  >
+                    <p>{item[0] && dayofweek[item[0].datetime.getDay()]}</p>
+                    <p>{item[0] && item[0].datetime.getDate()}</p>
+                  </div>
+                );
+              })}
           </div>
+
           <div className="flex items-center gap-3 w-full overflow-x-scroll mt-4">
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              10:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              10:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              11:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              11:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              12:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              12:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              13:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              13:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              14:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              14:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              15:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              15:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              16:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              16:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              17:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              17:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              18:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              18:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              19:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              19:30
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              20:00
-            </p>
-            <p className="text-sm font-light  flex-shrink-0 px-5 py-2 rounded-full cursor-pointer text-[#949494] border border-[#B4B4B4]">
-              20:30
-            </p>
+            {docSlots.length > 0 &&
+              docSlots[slotIndex].map((item, index) => {
+                return (
+                  <p
+                    onClick={() => setSlotTime(item.time)}
+                    key={index}
+                    className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer border ${
+                      slotTime === item.time
+                        ? "bg-primary text-white border-primary"
+                        : "text-[#949494] border-[#B4B4B4]"
+                    }`}
+                  >
+                    {item.time.toLowerCase()}
+                  </p>
+                );
+              })}
           </div>
+
           <button className="bg-primary text-white text-sm font-light px-20 py-3 rounded-full my-6">
             Book an appointment
           </button>
